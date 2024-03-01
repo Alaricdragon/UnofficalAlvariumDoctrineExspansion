174f63cac16d6c693d2a2f43ae78edfbf9d6f26c
Alaricdragon <alaric.meservy@gmail.com>
2024-03-01 15:57:39 -0700

    Initial commit

---
+++ jars/src/UnofficalAlvariumDoctrineExxpansion/data/scripts/UADE_startup.java
@@ -0,0 +1,13 @@
+package UnofficalAlvariumDoctrineExxpansion.data.scripts;
+import com.fs.starfarer.api.BaseModPlugin;
+
+public class UADE_startup extends BaseModPlugin {
+    @Override
+    public void onApplicationLoad() {
+    }
+
+    @Override
+    public void onGameLoad(boolean newGame) {
+        super.onGameLoad(newGame);
+    }
+}
